8b7fa88dd98aa6b721a354b173bcb8a6c45787a0
ILIA PETROV <ilia-p@yandex.ru>
2017-06-03 19:22:37 +0300

    Upgrading in accordace with recoomendation 2017-05-27 given by Evgeny Evseev

--- README.md
+++ README.md
@@ -1,7 +1,22 @@
-# Anti-Duplicator
+# **Anti-Duplicator**
 
-[The script explore a directory and returns duplicate files with their path]
+The script explore a directory and returns duplicate files with their path.
+You just need to input a path to a folder (like "C:\Users\HFLabs\Downloads")
+after launching and program will show duplicate file
 
-# Project Goals
+For instance, a result of script executing may be as follows:
+
+* C:\Users\HFLabs\Downloads
+* ('oraocci12d.dll', 'size = 1125888')
+* ('Address_REG_CLEANED_28042017.qvw', 'size = 32453632')
+* ('oraocci12.dll', 'size = 1099264')
+* ('asbs_all_client_final_fullfill_score.txt', 'size = 1441')
+* ('cvuhelper121.jar', 'size = 824883')
+* ('oraocci12.sym', 'size = 843632')
+* ('Address_LIVING_CLEANED_03052017.txt', 'size = 336168345')
+* ('cvuhelper102.jar', 'size = 34337')
+* ('Address_REG_CLEANED_28042017.txt', 'size = 218664359')
+
+# **Project Goals**
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,21 +1,29 @@
 import os
 from collections import defaultdict
+import argparse
 
-def duplicates(folder_to_explore):
-    root_dict = defaultdict(list)
+def file_list(folder_to_explore):
+    file_dict = defaultdict(list)
     for root, dirs, files in os.walk(folder_to_explore):
         for name in files:
             file_size = str(os.path.getsize(os.path.join(root,name)))
-            file_uniq = name + ', size=' + file_size
-            root_dict[file_uniq].append(root)
-    for item in root_dict:
-        if len(root_dict[item]) > 1:
-            quantity = len(root_dict[item])
-            print('\nФайл ', item, 'встречается ', quantity, ' раз(а) в папках:')
-            for path in root_dict[item]:
-                print(path)
+            file_name = (name, 'size = ' + file_size)
+            file_dict[file_name].append(root)
+    return file_dict
+    
+def duplicates(file_dict):
+    duplicates_list = [item for item in file_dict if len(file_dict[item]) > 1]
+    for item in duplicates_list:
+        print(item)
+    # for item in file_dict:
+    #     if len(file_dict[item]) > 1:
+    #         quantity = len(file_dict[item])
+    #         print('\nФайл ', item, 'встречается ', quantity, ' раз(а) в папках:')
+    #         for path in file_dict[item]:
+    #             print(path)
 
 if __name__ == '__main__':
     # pass
     folder_to_explore = input('Введите имя (путь до) папки\n')
-    duplicates(folder_to_explore)
+    file_dict = file_list(folder_to_explore)
+    duplicates(file_dict)
